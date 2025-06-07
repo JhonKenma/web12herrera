05ba41eec75d5306b2f278004f2eb27e4e395d71
Jhons Kenma <jhonskenma@gmail.com>
2025-06-07 03:22:35 -0500

    setimo cambio

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/api/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    
   ],
   theme: {
     extend: {},
